6fdf903ab04164a6b33a54a159e2bfa22019432d
PizzaLovers007 <trex@parkvue.com>
2025-12-01 23:22:54 -0600

    Day 2

---
+++ src/bin/day2_p1.rs
@@ -0,0 +1,46 @@
+use std::io::Read;
+
+use advent_of_code_2025::scanner::*;
+
+const SAMPLE: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+fn main() {
+    // let mut scan = Scanner::new(ScannerSource::Stdin);
+    let mut scan = Scanner::new(ScannerSource::File("input.txt".to_string()));
+
+    // let mut line = String::new();
+    // std::io::stdin().read_to_string(&mut line).unwrap();
+    // line = line.trim().to_string();
+
+    // let line = SAMPLE.to_string();
+
+    let line = scan.spar();
+
+    let split = line.split(',');
+    let keys: Vec<String> = split.map(|s| s.to_string()).collect();
+
+    let mut sum: i64 = 0;
+    for key in keys {
+        let (s1, s2) = key.split_once('-').unwrap();
+        let first: i64 = s1.parse().unwrap();
+        let last: i64 = s2.parse().unwrap();
+        for x in first..last+1 {
+            if !is_valid(&format!("{x}")) {
+                sum += x;
+                println!("{x}");
+            }
+        }
+    }
+
+    println!("{sum}");
+}
+
+fn is_valid(s: &str) -> bool {
+    if s.len()%2 == 1 {
+        return true;
+    }
+    let n = s.len();
+    let a = &s[0..n/2];
+    let b = &s[n/2..n];
+    return a != b;
+}
---
+++ src/bin/day2_p2.rs
@@ -0,0 +1,56 @@
+use std::io::Read;
+
+use advent_of_code_2025::scanner::*;
+
+const SAMPLE: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+fn main() {
+    // let mut scan = Scanner::new(ScannerSource::Stdin);
+    let mut scan = Scanner::new(ScannerSource::File("input.txt".to_string()));
+
+    // let mut line = String::new();
+    // std::io::stdin().read_to_string(&mut line).unwrap();
+    // line = line.trim().to_string();
+
+    // let line = SAMPLE.to_string();
+
+    let line = scan.spar();
+
+    let split = line.split(',');
+    let keys: Vec<String> = split.map(|s| s.to_string()).collect();
+
+    let mut sum: i64 = 0;
+    for key in keys {
+        let (s1, s2) = key.split_once('-').unwrap();
+        let first: i64 = s1.parse().unwrap();
+        let last: i64 = s2.parse().unwrap();
+        for x in first..last+1 {
+            if !is_valid(&format!("{x}")) {
+                sum += x;
+                println!("{x}");
+            }
+        }
+    }
+
+    println!("{sum}");
+}
+
+fn is_valid(s: &str) -> bool {
+    for n in 1..s.len() {
+        if s.len() % n != 0 {
+            continue;
+        }
+        let slices: Vec<&str> = (0..s.len()/n).map(|i| &s[i*n..(i+1)*n]).collect();
+        let mut diff = false;
+        for i in 0..slices.len() {
+            if slices[i] != slices[0] {
+                diff = true;
+                break;
+            }
+        }
+        if !diff {
+            return false;
+        }
+    }
+    return true;
+}
