fbcab22552bbf0fd2157e735f0fbeaef576ec5ff
PizzaLovers007 <trex@parkvue.com>
2025-12-03 23:52:31 -0600

    Day 4

---
+++ src/bin/day4_p1.rs
@@ -0,0 +1,55 @@
+use advent_of_code_2025::scanner::*;
+
+#[allow(dead_code)]
+const SAMPLE: &str = "
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+";
+
+fn main() {
+    // let mut scan = Scanner::new(ScannerSource::Stdin);
+    // let mut scan = Scanner::new(ScannerSource::Constant(SAMPLE));
+    let mut scan = Scanner::new(ScannerSource::File("input.txt"));
+
+    let mut mat: Vec<Vec<char>> = Vec::new();
+    while let Some(line) = scan.try_spar() {
+        mat.push(line.chars().collect());
+    }
+
+    let n = mat.len();
+    let m = mat[0].len();
+    let dr: [i32; 8] = [-1, -1, -1, 0, 1, 1, 1, 0];
+    let dc: [i32; 8] = [-1, 0, 1, 1, 1, 0, -1, -1];
+    let mut ans = 0;
+    for r in 0i32..n as i32 {
+        for c in 0i32..m as i32 {
+            if mat[r as usize][c as usize] != '@' {
+                continue;
+            }
+            let mut count = 0;
+            for (rr, cc) in dr.iter().map(|&x| r+x).zip(dc.iter().map(|&x| c+x)) {
+                let Ok(row) = usize::try_from(rr) else {
+                    continue;
+                };
+                let Ok(col) = usize::try_from(cc) else {
+                    continue;
+                };
+                if row < n && col < m && mat[row][col] == '@' {
+                    count += 1;
+                }
+            }
+            if count < 4 {
+                ans += 1;
+            }
+        }
+    }
+    println!("{ans}");
+}
---
+++ src/bin/day4_p2_better.rs
@@ -0,0 +1,63 @@
+use advent_of_code_2025::scanner::*;
+
+#[allow(dead_code)]
+const SAMPLE: &str = "
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+";
+
+fn main() {
+    // let mut scan = Scanner::new(ScannerSource::Stdin);
+    // let mut scan = Scanner::new(ScannerSource::Constant(SAMPLE));
+    let mut scan = Scanner::new(ScannerSource::File("input.txt"));
+
+    let mut mat: Vec<Vec<char>> = Vec::new();
+    while let Some(line) = scan.try_spar() {
+        mat.push(line.chars().collect());
+    }
+
+    let n = mat.len();
+    let m = mat[0].len();
+    let dr: [i32; 8] = [-1, -1, -1, 0, 1, 1, 1, 0];
+    let dc: [i32; 8] = [-1, 0, 1, 1, 1, 0, -1, -1];
+    let deltas: Vec<_> = dr.into_iter().zip(dc.into_iter()).collect();
+    let mut ans = 0;
+    loop {
+        let mut mat_copy = mat.clone();
+        let mut found = false;
+        for r in 0..n {
+            for c in 0..m {
+                if mat[r][c] != '@' {
+                    continue;
+                }
+                let count = deltas
+                    .iter()
+                    .map(|&(dr, dc)| (r as i32 + dr, c as i32 + dc))
+                    .filter(|&(row, col)| {
+                        (0..n as i32).contains(&row) && (0..m as i32).contains(&col)
+                    })
+                    .map(|(row, col)| mat[row as usize][col as usize])
+                    .filter(|&c| c == '@')
+                    .count();
+                if count < 4 {
+                    mat_copy[r][c] = '.';
+                    ans += 1;
+                    found = true;
+                }
+            }
+        }
+        mat = mat_copy;
+        if !found {
+            break;
+        }
+    }
+    println!("{ans}");
+}
