658c1b3c1bdd9e583fbf4744e450beb7143bb3a1
PizzaLovers007 <trex@parkvue.com>
2025-12-04 23:24:02 -0600

    Day 5

---
+++ src/bin/day5_p1.rs
@@ -0,0 +1,48 @@
+use advent_of_code_2025::scanner::*;
+
+#[allow(dead_code)]
+const SAMPLE: &str = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+
+fn main() {
+    // let mut scan = Scanner::new(ScannerSource::Stdin);
+    // let mut scan = Scanner::new(ScannerSource::Constant(SAMPLE));
+    let mut scan = Scanner::new(ScannerSource::File("input.txt"));
+
+    scan.set_delimiter('\n');
+
+    let mut ranges: Vec<_> = Vec::new();
+    let mut state = 0;
+    let mut ans = 0;
+    for line in scan.into_iter() {
+        let s = String::from(line);
+        if s.is_empty() {
+            state = 1;
+            println!("{ranges:?}");
+            continue;
+        }
+        if state == 0 {
+            let (left, right) = s.split_once('-').unwrap();
+            ranges.push(left.parse::<i64>().unwrap()..right.parse::<i64>().unwrap()+1);
+        } else {
+            let x = s.parse().unwrap();
+            for r in ranges.iter() {
+                if r.contains(&x) {
+                    ans += 1;
+                    break;
+                }
+            }
+        }
+    }
+    println!("{ans}");
+}
---
+++ src/bin/day5_p2.rs
@@ -0,0 +1,71 @@
+use advent_of_code_2025::scanner::*;
+
+#[allow(dead_code)]
+const SAMPLE: &str = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+";
+
+fn main() {
+    // let mut scan = Scanner::new(ScannerSource::Stdin);
+    let mut scan = Scanner::new(ScannerSource::Constant(SAMPLE));
+    // let mut scan = Scanner::new(ScannerSource::File("input.txt"));
+
+    scan.set_delimiter('\n');
+
+    let mut lefts: Vec<_> = Vec::new();
+    let mut rights: Vec<_> = Vec::new();
+    for line in scan.into_iter() {
+        let s = String::from(line);
+        if s.is_empty() {
+            break;
+        }
+        let (left, right) = s.split_once('-').unwrap();
+        lefts.push(left.parse::<i64>().unwrap());
+        rights.push(right.parse::<i64>().unwrap());
+    }
+    lefts.sort();
+    rights.sort();
+    let mut ans = 0;
+    let mut count = 0;
+    let mut l = 0;
+    let mut r = 0;
+    let mut prev = -1;
+    while l < lefts.len() && r < rights.len() {
+        let left = lefts[l];
+        let right = rights[r];
+        if left <= right {
+            count += 1;
+            l += 1;
+            if count == 1 {
+                prev = left;
+            }
+        } else {
+            count -= 1;
+            r += 1;
+            if count == 0 {
+                ans += right - prev + 1;
+            }
+        }
+        // println!("{left} {right} {count} {ans}");
+    }
+    while r < rights.len() {
+        let right = rights[r];
+        let curr = right;
+        count -= 1;
+        r += 1;
+        if count == 0 {
+            ans += curr - prev + 1;
+        }
+        // println!("--- {right} {count} {ans}");
+    }
+    println!("{ans}");
+}
